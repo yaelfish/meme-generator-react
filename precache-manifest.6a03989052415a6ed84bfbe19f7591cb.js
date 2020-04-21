414e8386d8dd73e31fda7a03fc8ccaa5b440657a
yaelfish <winkwinksituation@gmail.com>
2020-04-22 00:21:27 +0300

    Updates

---
+++ precache-manifest.6a03989052415a6ed84bfbe19f7591cb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1710973bde60c40b08607e57a06cf2b2",
+    "url": "/meme-generator-react/index.html"
+  },
+  {
+    "revision": "e605fb3b85a501418be3",
+    "url": "/meme-generator-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "42815eecdfac668066d4",
+    "url": "/meme-generator-react/static/css/main.6647bd64.chunk.css"
+  },
+  {
+    "revision": "e605fb3b85a501418be3",
+    "url": "/meme-generator-react/static/js/2.3a962c69.chunk.js"
+  },
+  {
+    "revision": "150e7ebf1b16402b62fb665d1a7869df",
+    "url": "/meme-generator-react/static/js/2.3a962c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42815eecdfac668066d4",
+    "url": "/meme-generator-react/static/js/main.c3fd6276.chunk.js"
+  },
+  {
+    "revision": "22db2766a4e88af2aa8e",
+    "url": "/meme-generator-react/static/js/runtime-main.7b93e446.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/meme-generator-react/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "60f6b3e809465733aac5d50f3b236766",
+    "url": "/meme-generator-react/static/media/meme-welcome.60f6b3e8.jpeg"
+  }
+]);
